904f08afaeb7aa9367e0cd0d1ba140c0114b8bfc
Sailtor <Klimentenko02@mail.ru>
2023-06-11 14:35:10 +0300

    Added out dtos
    Added out dtos in **Models\Dtos\Out** folder

--- Models/Dtos/In/UserForCreationDto.cs
+++ Models/Dtos/In/UserForCreationDto.cs
@@ -2,7 +2,6 @@
 {
     public class UserForCreationDto
     {
-        public string RoleName { get; set; }
         public string Login { get; set; } = null!;
         public string Password { get; set; } = null!;
         public string Email { get; set; } = null!;
