e0bdabe5cea72dc036344ec3239602346fa4e43f
Sailtor <Klimentenko02@mail.ru>
2023-06-11 14:39:21 +0300

    Added out dtos
    Added out dtos in **Models\Dtos\Out** folder

---
+++ Models/Dtos/Out/PurchaseForResponceDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GameStoreWebAPI.Models.Dtos.Out
+{
+    public class PurchaseForResponceDto
+    {
+        public int UserId { get; set; }
+        public int GameId { get; set; }
+        public DateTime PurchaseDate { get; set; }
+    }
+}
