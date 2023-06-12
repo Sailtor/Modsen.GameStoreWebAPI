b58671e289ff51b59c6ab62e560444a76c5b0c20
Sailtor <Klimentenko02@mail.ru>
2023-06-12 19:24:04 +0300

    Added API token and tokens support

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
 using GameStoreWebAPI.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,21 +14,40 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Description = "Standard Authorization header using the Bearer scheme (\"bearer {token}\")",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
-if (builder.Environment.IsDevelopment())
-{
-    builder.Services.AddDbContext<GameStoreDBContext>(
-        opt => opt.UseSqlServer(
-            builder.Configuration.GetConnectionString("LocalDb")));
-}
-else
-{
-    builder.Services.AddDbContext<GameStoreDBContext>(
-            opt => opt.UseSqlServer(
-                builder.Configuration.GetConnectionString("ProductionDb")));
-}
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+                .GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+
+builder.Services.AddDbContext<GameStoreDBContext>(
+    opt => opt.UseSqlServer(
+        builder.Configuration.GetConnectionString(
+            builder.Environment.IsDevelopment() ? "LocalDb" : "ProductionDb")));
 
 var app = builder.Build();
 
@@ -36,6 +60,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
