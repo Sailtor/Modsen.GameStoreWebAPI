c9642f8b26e68ad7a6f2f58cc7de75b9a76ce0d5
Sailtor <Klimentenko02@mail.ru>
2023-06-18 16:48:04 +0300

    Restructured project to include several projects

---
+++ DAL/Repository/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+
+namespace DAL.Repository
+{
+    internal interface IRepository<TEntity,TId> 
+        where TEntity : class 
+        where TId : class
+    {
+        Task<TEntity> GetAsync(TId id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity,bool>> predicate);
+
+        Task AddAsync(TEntity entity);
+        Task AddRangeAsync(IEnumerable<TEntity> entities);
+
+        Task DeleteAsync(TEntity entity);
+        Task DeleteRangeAsync(IEnumerable<TEntity> entities);
+    }
+}
