ceb3c42b3832d94e5cbba57ab4623d39b76d0d89
BulbaLove <bulbalove5051@gmail.com>
2023-06-20 19:48:14 +0300

    Puchases, Genres, Delopers Services Interface

---
+++ BLL/Services/Contracts/IPurchaseService.cs
@@ -0,0 +1,15 @@
+﻿using BLL.Dtos.InDto;
+using BLL.Dtos.OutDto;
+using DAL.Models;
+using DAL.Repository.UnitOfWork;
+
+namespace BLL.Services.Contracts
+{
+    public interface IPurchaseService
+    {
+        public Task<IEnumerable<PurchaseForResponceDto>> GetUserPurchasesAsync(int userid);
+        public Task<PurchaseForResponceDto> GetUserPurchaseByIdAsync(int usergameid);
+        public Task AddUserPurchaseAsync(PurchaseForCreationDto purchase, int usergameid);
+        public Task DeleteUserPurchaseAsync(int usergameid);
+    }
+}
